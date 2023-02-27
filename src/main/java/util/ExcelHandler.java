49ea2eaee40a908cb2c9a30904ee4b487cae700b
harunahb <95856676+harunahb@users.noreply.github.com>
2023-02-27 19:54:21 +0530

    Update ExcelHandler.java

--- src/main/java/util/ExcelHandler.java
+++ src/main/java/util/ExcelHandler.java
@@ -1,19 +1,30 @@
 package util;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelHandler {
-    public static synchronized void createExcelReport(String filepath, List<String> header) {
-        File file = new File(filepath);
+    public static synchronized void createExcelReport(String dir, String filename, List<String> header) {
+        File file = new File(dir + "/" + filename);
         if (!file.exists()) {
+
+            File dirFile = new File(dir);
+            if (!dirFile.exists()) {
+                if(dirFile.mkdirs()) {
+                    throw new RuntimeException("unable to create dir " + dir);
+                }
+            }
+
             XSSFWorkbook workbook = new XSSFWorkbook();
             XSSFSheet sheet = workbook.createSheet("sheet1");
             XSSFRow row = sheet.createRow(0);
@@ -35,10 +46,11 @@ public class ExcelHandler {
     }
 
     public static synchronized void writeExcel(String filepath, ArrayList<String> data) {
-        File file = new File(filepath);
-        if (!file.exists()) {
-            XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.getSheetAt(0);
+        try {
+            FileInputStream fis = new FileInputStream(filepath);
+
+            Workbook workbook = WorkbookFactory.create(fis);
+            XSSFSheet sheet = (XSSFSheet) workbook.getSheetAt(0);
             int lastRowNum = sheet.getLastRowNum();
 
             XSSFRow row = sheet.createRow(lastRowNum+1);
@@ -47,14 +59,11 @@ public class ExcelHandler {
                 cell.setCellValue(data.get(i));
             }
 
-            try {
-                FileOutputStream out = new FileOutputStream(file);
-                workbook.write(out);
-                out.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+            FileOutputStream out = new FileOutputStream(filepath);
+            workbook.write(out);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
